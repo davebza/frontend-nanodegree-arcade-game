2935388712c1a650424118a9d8095e0dbcc13aa7
Dave Brown <daveb.za@gmail.com>
2017-03-09 11:39:16 +0800

    Gor the game to draw, playr char init, 3 enemies moving at random speeds

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
+//random generator for numbers. Should it be part of the enemy object definition?
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
+    this.speed = getRandomNumber(100, 300);
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +22,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x+(this.speed * dt);
+    return this.x;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +34,42 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
+    this.speed = 100;
+    this.sprite = 'images/char-horn-girl.png';
+};
+
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Place the player object in a variable called player - home square is 200, 430
+var player = new Player(200, 430);
+var allEnemies = [];
 
-
+// enemy start positions: top row: 0, 50; mid row: 0, 140; bottom row: 0, 220;
+var enemy1 = new Enemy(0, 220);
+var enemy2 = new Enemy(0, 50);
+var enemy3 = new Enemy(0, 140);
+allEnemies.push(enemy1, enemy2, enemy3);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
